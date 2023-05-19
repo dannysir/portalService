0994b5c0f76042d9531e47edb3e101c934034dbc
Seo San <dannysir@naver.com>
2023-05-19 22:46:10 +0900

    tomcat & upload

--- userdao/src/main/java/kr/ac/jejunu/user/JdbcContext.java
+++ userdao/src/main/java/kr/ac/jejunu/user/JdbcContext.java
@@ -10,57 +10,7 @@ public class JdbcContext {
         this.dataSource = dataSource;
     }
 
-    public void jdbcContextForInsert(User user, StatementStrategy statementStrategy) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-            resultSet = preparedStatement.getGeneratedKeys();
-            resultSet.next();
-            user.setId(resultSet.getLong(1));
-        } finally {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void jdbcContextForUpdate(StatementStrategy statementStrategy) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-        } finally {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            try {
-                connection.close();
-            }catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-    public User jdbcContextForFind(StatementStrategy statementStrategy) throws SQLException {
+    User jdbcContextForFind(StatementStrategy statementStrategy) throws SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -68,9 +18,8 @@ public class JdbcContext {
         try {
             connection = dataSource.getConnection();
             preparedStatement = statementStrategy.makeStatement(connection);
-
             resultSet = preparedStatement.executeQuery();
-            if(resultSet.next()) {
+            if (resultSet.next()) {
                 user = new User();
                 user.setId(resultSet.getLong("id"));
                 user.setName(resultSet.getString("name"));
@@ -96,17 +45,56 @@ public class JdbcContext {
         return user;
     }
 
-    void insert(User user, Object[] params, String sql, UserDao userDao) throws SQLException {
-        StatementStrategy statementStrategy = connection -> {
-            PreparedStatement preparedStatement = connection.prepareStatement
-                    (sql
-                            , Statement.RETURN_GENERATED_KEYS);
-            for (int i = 0; i < params.length; i++){
-                preparedStatement.setObject(i+1, params[i]);
+    void jdbcContextForInsert(User user, StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getLong(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-            return preparedStatement;
-        };
-        jdbcContextForInsert(user, statementStrategy);
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void jdbcContextForUpdate(StatementStrategy statementStrategy) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection);
+            preparedStatement.executeUpdate();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     void update(String sql, Object[] params) throws SQLException {
@@ -120,13 +108,24 @@ public class JdbcContext {
         jdbcContextForUpdate(statementStrategy);
     }
 
-    User find(String sql, Object[] params) throws SQLException {
+    void insert(User user, String sql, Object[] params) throws SQLException {
         StatementStrategy statementStrategy = connection -> {
-            PreparedStatement preparedStatement = connection.prepareStatement
-                    (sql);
-            for(int i = 0; i < params.length; i++){
+            PreparedStatement preparedStatement = connection.prepareStatement (sql, Statement.RETURN_GENERATED_KEYS);
+            for(int i = 0; i< params.length; i++) {
                 preparedStatement.setObject(i+1, params[i]);
             }
+            return preparedStatement;
+        };
+        jdbcContextForInsert(user, statementStrategy);
+    }
+
+    User find(String sql, Object[] params) throws SQLException {
+        StatementStrategy statementStrategy = connection -> {
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setObject(i + 1, params[i]);
+            }
+
             return preparedStatement;
         };
         return jdbcContextForFind(statementStrategy);
--- userdao/src/main/java/kr/ac/jejunu/user/User.java
+++ userdao/src/main/java/kr/ac/jejunu/user/User.java
@@ -10,30 +10,5 @@ public class User {
     private Long id;
     private String name;
     private String password;
-    //@Data를 사용하면 순환호출이 되면서 문제 발생
-    //private User user;
-
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public void setName(String name) {
-//        this.name = name;
-//    }
-//
-//    public String getPassword() {
-//        return password;
-//    }
-//
-//    public void setPassword(String password) {
-//        this.password = password;
-//    }
+    private User user;
 }
